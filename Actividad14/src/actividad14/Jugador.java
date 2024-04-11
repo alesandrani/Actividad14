3366e81f63fe214f064d9a78cd6449e74f6afb9f
alesandrani <alesandrani@outlook.es>
2024-04-11 12:35:46 +0200

    clase jugador

--- Actividad14/src/actividad14/Jugador.java
+++ Actividad14/src/actividad14/Jugador.java
@@ -5,47 +5,27 @@ public class Jugador {
 	private int numeroTarjetasAmarillas;
 	private int numeroTarjetasRojas;
 
-                  
-
 //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+	public void ponerDorsal(int dorsal) {
 
+		if (dorsal >= 1 && dorsal <= 30) {
+			this.dorsal = dorsal;
+		} else {
+			this.dorsal = -1;
+		}
+	}
 
+	public boolean estaExpulsado() {
 
-     public void ponerDorsal(int dorsal) {
+		boolean expulsado = false;
+		if (numeroTarjetasAmarillas == 2) {
+			expulsado = true;
+		}
+		if (numeroTarjetasRojas == 1) {
+			expulsado = true;
+		}
+		return expulsado;
 
-                   if(dorsal >= 1 && dorsal <= 30) {
-
-                                 this.dorsal = dorsal;
-
-                   }else {
-
-                                 this.dorsal = -1;
-
-                   }
-
-     }
-
-    
-
-     public boolean estaExpulsado() {
-
-                   boolean expulsado = false;                                                  
-
-                   if(numeroTarjetasAmarillas == 2) {
-
-                                 expulsado = true;
-
-                   }                          
-
-                   if(numeroTarjetasRojas == 1) {
-
-                                 expulsado = true;
-
-                   }                          
-
-                   return expulsado;
-
-     }            
+	}
 
 }
-
