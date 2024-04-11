4a8a1efbeff8a5447ffe39e23513485bc86b4326
Avril2504 <avrilpino@gmail.com>
2024-04-11 13:40:19 +0200

    documentacion Avril

--- Actividad14/src/actividad14/Jugador.java
+++ Actividad14/src/actividad14/Jugador.java
@@ -1,11 +1,44 @@
 package actividad14;
 
+/**
+ * El objetivo de esta clase es <b>representar un jugador</b>
+ * 
+ * Tendra <b>metodos</b> que nos permitira saber cosas del jugador
+ * como el número de camiseta con el metodo <b>"ponerDorsal"</b> y
+ * saber si esta expulsado co el metodo <b>"estaExpulsado"</b>.
+ * 
+ * @author Avril
+ */
 public class Jugador {
+	/**
+	 * "dorsal" representa el número de la camiseta del jugador.
+	 * Este es utilizado para crear parte del metodo "ponerDorsal"
+	 */
 	private int dorsal;
+	/**
+	 * "numeroTarjetasAmarillas" representa el número de taretas 
+	 * amarillas asignadas al jugador.
+	 * Se utiliza este atributo en el metodo "estaExpulsado"
+	 */
 	private int numeroTarjetasAmarillas;
+	/**
+	 * "numeroTarjetasROjas" representa el número de taretas 
+	 * rojas asignadas al jugador.
+	 * Se utiliza este atributo en el metodo "estaExpulsado"
+	 */
 	private int numeroTarjetasRojas;
 
 //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+	
+	/**
+	 * Metodo que <b>coloca</b> el número de dorsal pasado por parametro
+	 * al atributo "dorsal". La condicion es que si el número del 
+	 * dorsal pasado por parametro es mayor o igual a 1 y 
+	 * menor o igual a 30 se le asignara dicho número al atributo 
+	 * "dorsal". En caso contrario el atributo "dorsal" se igualara 
+	 * a "-1"
+	 * @param dorsal Número de camiseta del jugador pasador por parametro
+	 */
 	public void ponerDorsal(int dorsal) {
 
 		if (dorsal >= 1 && dorsal <= 30) {
@@ -15,6 +48,16 @@ public class Jugador {
 		}
 	}
 
+	/**
+	 * Metodo que <b>devuelve</b> si un jugador a sido expulsado o no. En primer lugar
+	 * se crea la variable expulsado y se iguala a "false". <b>La condicion</b> es que
+	 * si el atributo "numeroTarjetasAmarillas" es igual a 2 el jugador estara
+	 * expulsado. Ademas si el atributo "numeroTarjetasRojas" es igual a 1 tambien
+	 * sera expulsado. En caso que <b>no</b> se cumpla la condicion anterior devolvera
+	 * que no esta expulsado
+	 *  
+	 * @return true si el jugador esta expulsado o false si no estaexpulsado
+	 */
 	public boolean estaExpulsado() {
 
 		boolean expulsado = false;
--- Actividad14/src/actividad14/Soldado.java
+++ Actividad14/src/actividad14/Soldado.java
@@ -1,10 +1,33 @@
 package actividad14;
 
+/**
+ * El objetivo de esta clase es <b>representar a un soldado</b> y saber
+ * de este si esta muerto y su numero de balas
+ * 
+ * Esta clase tendra metodos tales como "puedeDisparar" y
+ * "disparar
+ * 
+ * @author Avril
+ */
 public class Soldado {
+	/**
+	 * Este atributo representa si un soldado esta muerto o no
+	 */
 	private boolean estaMuerto;
+	/**
+	 * Este atributo representa el número de balas que tiene
+	 * un soldado
+	 */
 	private int numeroBalas;
 
 //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+	
+	/**
+	 * Metodo que <b>devuelve</b> si un soldado puede disparar o no. Un soldado
+	 * puede disparar  si su numero de balas es mayor a 0. En caso contrario
+	 * no podra disparar.
+	 * @return <b>true</b> si el soldado puede disparar o <b>false</b> si no puede disparar
+	 */
 	public boolean puedeDisparar() {
 		if (this.numeroBalas > 0) {
 			return true;
@@ -13,6 +36,12 @@ public class Soldado {
 
 	}
 
+	/**
+	 * Metodo que <b>mata</b> a un soldado pasado por parametro. El
+	 * número de balas disminuira y el atributo estaMuerto del
+	 * soldado pasado por parametro sera igual a true
+	 * @param sol soldado pasado por parametro
+	 */
 	public void disparar(Soldado sol) {
 		this.numeroBalas--;
 		sol.estaMuerto = true;
