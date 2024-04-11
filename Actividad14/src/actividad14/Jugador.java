d24832c06ab4c02e95bda313e75e2f1f312b2747
Yina Hernandez <hernandezyinary@gmail.com>
2024-04-11 13:33:48 +0200

    Documentacion de la clase jugador y soldado

--- Actividad14/src/actividad14/Jugador.java
+++ Actividad14/src/actividad14/Jugador.java
@@ -1,13 +1,60 @@
 package actividad14;
 
+/**
+ * Clase que contine el objeto jugador, tiene como objetivo 
+ * realizar un seguimiento de las diferentes estadisticas del
+ * jugador. Contiene los siguientes atributos:
+ * <br>
+ * <ul>
+ * <li> dorsal </li>
+ * <li> numeroTarjetasAmarillas </li>
+ * <li> numeroTarjetasRojas </li>
+ * </ul>
+ * <br>
+ * Posee los siguientes metodos {@link #ponerDorsal(int)},
+ * {@link #numeroTarjetasAmarillas} y {@link #numeroTarjetasRojas}
+ * @author Yina.
+ */
 public class Jugador {
+	/**
+	 * Representa el numero de la camiseta del jugador.
+	 * Se limita su rango de valores con el metodo 
+	 * {@link #ponerDorsal(int)}
+	 * Atributo de tipo entero
+	 */
 	private int dorsal;
+	/**
+	 * Representa el numero de tarjetas amarillas sacadas al
+	 * jugador.
+	 * Atributo de tipo entero
+	 */
 	private int numeroTarjetasAmarillas;
+	/**
+	 * Representa el numero de tarjetas rojas sacadas al
+	 * jugador.
+	 * Atributo de tipo entero
+	 */
 	private int numeroTarjetasRojas;
 
-//Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+//Crear los métodos “get” y “set” de los atributos cuando se vayan 
+//a hacer las pruebas y la documentación. Aquí no se han creado 
+//porque no aportan nada
+	
+	/**
+	 * Metodo que condiciona el rango de valores que puede
+	 * tener el dorsal del jugador, teniendo como parametro de 
+	 * entrada un dorsal. 
+	 * <br>
+	 * La condicion que sigue es la siguiente: 
+	 * - Si el dorsal es mayor o igual a 1 y menor o igual a 30: El
+	 * atributo dorsal del jugador se igualara al dorsal pasado por 
+	 * parametro
+	 * - De no cumplirse la condicion anterior el atributo dorsal
+	 * se igualara a "-1"
+	 * 
+	 * @param dorsal valor que se condicionara.
+	 */
 	public void ponerDorsal(int dorsal) {
-
 		if (dorsal >= 1 && dorsal <= 30) {
 			this.dorsal = dorsal;
 		} else {
@@ -15,8 +62,19 @@ public class Jugador {
 		}
 	}
 
+	/**
+	 * Metodo que devulve si el jugador será o no expulsado, creando 
+	 * una variable llamada <i>'expulsado'</i> e igualandola a 'false'. 
+	 * <br>
+	 * Las condiciones que rigen el metodo son:
+	 * -En caso de que el jugador hubiese recibido dos tarjetas amarillas
+	 * seria expulsado.
+	 * -Si el jugador recibio directamente una tarjeta roja tambien va a 
+	 * ser expulsado. 
+	 * @return <b>true</b> si el jugador ha recibido dos tarjetas amarillas
+	 * o una roja y <b>false</b> si no secedio ninguno de los casos anteriores.
+	 */
 	public boolean estaExpulsado() {
-
 		boolean expulsado = false;
 		if (numeroTarjetasAmarillas == 2) {
 			expulsado = true;
--- Actividad14/src/actividad14/Soldado.java
+++ Actividad14/src/actividad14/Soldado.java
@@ -1,11 +1,42 @@
 package actividad14;
 
+/**
+ * Clase que contine el objeto soldado, tiene como objetivo 
+ * atacar a otros soldados.
+ * Contiene los siguientes atributos:
+ * <br>
+ * <ul>
+ * <li> estaMuerto </li>
+ * <li> numeroBalas </li>
+ * </ul>
+ * <br>
+ * Tambien posee los siguientes metodos {@link #puedeDisparar)},
+ * {@link #disparar(Soldado)}
+ * @author Yina.
+ * */
 public class Soldado {
+	/**
+	 * Representa si el soldado esta o no Muerto
+	 * Atributo de tipo boolean
+	 */
 	private boolean estaMuerto;
+	/**
+	 * Representa el numero de balas que tiene el soldado
+	 * Atributo de tipo entero
+	 */
 	private int numeroBalas;
 
-//Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
-	public boolean puedeDisparar() {
+    //Crear los métodos “get” y “set” de los atributos cuando se vayan
+	//a hacer las pruebas y la documentación. Aquí no se han creado porque no
+	//aportan nada
+	
+	/**
+	 * Metodo que devulve si el Soldado puede disparar o no
+	 * evaluando el atributo numeroBalas 
+	 * @return <b>true</b> Si el atributo numeroBalas es mayor a 0 y
+	 * <b>false</b> si no es mayor a 0.
+	 */
+	public boolean puedeDisparar(){
 		if (this.numeroBalas > 0) {
 			return true;
 		}
@@ -13,9 +44,15 @@ public class Soldado {
 
 	}
 
+	/**
+	 * Metodo mediante el cual se mata a un Soldado pasado por
+	 * parametro. Se le restara una bala al soldado desde el 
+	 * cual se llama el metodo y se igualará el atributo
+	 * estaMuerto del soldado pasado por parametro a 'true'
+	 * @param sol representa el soldado que morirá.
+	 */
 	public void disparar(Soldado sol) {
 		this.numeroBalas--;
 		sol.estaMuerto = true;
-
 	}
 }
