176524956a6231884cdea819dd43f02dfb5a0de5
elenitaferr <fernandezcanizarese@gmail.com>
2024-04-11 13:51:48 +0200

    add ejercicios

--- Actividad14/src/actividad14/Jugador.java
+++ Actividad14/src/actividad14/Jugador.java
@@ -1,9 +1,14 @@
 package actividad14;
 /**
+<<<<<<< HEAD
  * Clase que incluye dos métodos con los que conoceremos el dorsal del <b>Jugador</b>
  *  mediante el método <b>"ponerDorsal"</b>y esi está o no expulsado mediante el método <b>"estaExpulsado"</b>
  *  @author Felix de Pablo
  *  @version 1.0
+=======
+ * El obejetivo de esta clase es proporcionar lo que va a tener cada jugador
+ * Tendra diferentes metodos para ver las tarjetas rojas y amarillas y dorsal
+>>>>>>> 977dadbbef5b3607214a34a03eba1a896dfbb0c5
  */
 public class Jugador {
 	/**
@@ -19,11 +24,45 @@ public class Jugador {
 	 */
 	private int numeroTarjetasRojas;
 
+<<<<<<< HEAD
 //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
 	/**
 	 * Este método nos ayudará a conocer el dorsal pasado por parámetros del <b>Jugador</b>
 	 * El número deberá comprender entre el 1 y el 30.
 	 * @param <b>dorsal</b> el número pasado por parámetro que llevará el jugador a la espalda. 
+=======
+public int getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+
+	public int getNumeroTarjetasAmarillas() {
+		return numeroTarjetasAmarillas;
+	}
+
+	public void setNumeroTarjetasAmarillas(int numeroTarjetasAmarillas) {
+		this.numeroTarjetasAmarillas = numeroTarjetasAmarillas;
+	}
+
+	public int getNumeroTarjetasRojas() {
+		return numeroTarjetasRojas;
+	}
+
+	public void setNumeroTarjetasRojas(int numeroTarjetasRojas) {
+		this.numeroTarjetasRojas = numeroTarjetasRojas;
+	}
+
+	//Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación.
+	//Aquí no se han creado porque no aportan nada
+	
+	/**
+	 * Este metodo es una restriccion para cuando se crea un metodo para asignar el numero de camiseta a cada jugador.
+	 * Los numeros tienen que estar en el rango de <b>(1-30 incluidos)<b>. Si sale del rango  dara error.
+	 * @param dorsal el numero que se le asignara al jugador
+>>>>>>> 977dadbbef5b3607214a34a03eba1a896dfbb0c5
 	 */
 	public void ponerDorsal(int dorsal) {
 
@@ -33,6 +72,7 @@ public class Jugador {
 			this.dorsal = -1;
 		}
 	}
+<<<<<<< HEAD
 	/**
 	 * Este método nos permite saber si el jugador está o no expulsado.
 	 * Si el jugador tiene un número de tarjetas amarillas  <b>("numeroTarjetasAmarillas")</b>igual a 2 estará expulsado.
@@ -40,6 +80,13 @@ public class Jugador {
 	 * el jugador también estará expulsado.
 	 * @return <b>true</b> si el jugador está expulsado. <b>false</b> si el jugador no está expulsado.
 	 */
+=======
+/**
+ * Este metodo nos devulve si un jugador va a ser expulsado. Cuando el jugador llega a 2 tarjetas amarillas sera expulsado al igual
+ * que si tiene una tarjeta roja sera expulsado tambien.
+ * @return true - expulsado; false - sigue jugando
+ */
+>>>>>>> 977dadbbef5b3607214a34a03eba1a896dfbb0c5
 	public boolean estaExpulsado() {
 
 		boolean expulsado = false;
--- Actividad14/src/actividad14/Soldado.java
+++ Actividad14/src/actividad14/Soldado.java
@@ -1,10 +1,15 @@
 package actividad14;
 /**
+<<<<<<< HEAD
  * Esta clase incluye dos métodos que darán funcionalidad al <b>Soldado</b>.
  * El primer método nos dirá si al soldado le quedan balas o no.
  * Con el segundo método el soldado disparará si le quedan balas.
  * @author Felix de Pablo
  * @version 1.0
+=======
+ * esta clase nos mostarara la cantidad de balas que va tener el soldado para crear los metodos
+ *  que nos ayudaran saber si un soldado vive o muere
+>>>>>>> 977dadbbef5b3607214a34a03eba1a896dfbb0c5
  */
 public class Soldado {
 	/**
@@ -16,6 +21,7 @@ public class Soldado {
 	 */
 	private int numeroBalas;
 
+<<<<<<< HEAD
 //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
 	/**
 	 * Este método nos indica si el <b>Soldado</b> puede o no disparar, en función del número de balas que se hayan
@@ -24,6 +30,31 @@ public class Soldado {
 	 * En caso de que sea igual a 0 <b>no</b> podrá disparar.
 	 * @return<b>true</b> si le quedan balas y puede disparar o <b>false</b> si no le quedan balas 
 	 * no puede disparar.
+=======
+public boolean isEstaMuerto() {
+		return estaMuerto;
+	}
+
+	public void setEstaMuerto(boolean estaMuerto) {
+		this.estaMuerto = estaMuerto;
+	}
+
+	public int getNumeroBalas() {
+		return numeroBalas;
+	}
+
+	public void setNumeroBalas(int numeroBalas) {
+		this.numeroBalas = numeroBalas;
+	}
+
+	//Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación.
+	//Aquí no se han creado porque no aportan nada
+	
+	/**
+	 * Este metodo nos devulve si podemos disparar o no. Si el numero de balas es superior a 0 podemos disparar 
+	 * en caso contrario no
+	 * @return true - dispara. false - no se puede disparar(no hay balas)
+>>>>>>> 977dadbbef5b3607214a34a03eba1a896dfbb0c5
 	 */
 	public boolean puedeDisparar() {
 		if (this.numeroBalas > 0) {
@@ -32,12 +63,20 @@ public class Soldado {
 		return false;
 
 	}
+<<<<<<< HEAD
 	/**
 	 * Este método sirve para que el soldado dispare. Cuando el soldado dispara, gasta balas,
 	 * por lo tanto se le resta una bala al número de balas que le habíamos pasado por parámetro.
 	 * Además al objeto sol pasado por parámetro que reciba el disparo morirá.
 	 * @param <b>sol</b> el objeto sol pasado por parámetro al que queremos disparar.
 	 */
+=======
+/**
+ * Este metodo evalua cuando muere un soldado.
+ * invocando este metodo se comieza la lucha el soldado que se quede primero sin balas muere.
+ * @param sol objeto soldado
+ */
+>>>>>>> 977dadbbef5b3607214a34a03eba1a896dfbb0c5
 	public void disparar(Soldado sol) {
 		this.numeroBalas--;
 		sol.estaMuerto = true;
